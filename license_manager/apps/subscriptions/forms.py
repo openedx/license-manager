a1cbfd6b5ec1f392eb1b6608bf9484836f538fad
chavesj <jchaves@edx.org>
2020-05-28 13:09:27 -0400

    raising ValidationError if new number of licences is less than initial number

--- license_manager/apps/subscriptions/forms.py
+++ license_manager/apps/subscriptions/forms.py
@@ -5,6 +5,8 @@ Forms to be used in the subscriptions django app.
 from __future__ import absolute_import, unicode_literals
 
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext
 
 from license_manager.apps.subscriptions.models import License, SubscriptionPlan
 
@@ -14,15 +16,23 @@ class SubscriptionPlanForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(SubscriptionPlanForm, self).__init__(*args, **kwargs)
-        calc_num_licenses = 0
+        self.calc_num_licenses = 0
         if hasattr(self, 'instance'):
-            calc_num_licenses = self.instance.calc_num_licenses
-        self.fields['num_licenses'].initial = calc_num_licenses
+            self.calc_num_licenses = self.instance.calc_num_licenses
+        self.fields['num_licenses'].initial = self.calc_num_licenses
 
     def save(self, commit=True):
         num_licenses = self.cleaned_data.get('num_licenses', None)
+        if num_licenses < self.calc_num_licenses:
+            raise ValidationError(
+                gettext('Invalid value: %(value)s'),
+                code='cannot decrease num_licenses',
+                params={'value': num_licenses}
+            )
+        else:
+            num_new_licenses = num_licenses - self.calc_num_licenses
         subscription_uuid = super(SubscriptionPlanForm, self).save(commit=commit)
-        for _ in range(num_licenses):
+        for _ in range(num_new_licenses):
             lic = License(subscription_plan=subscription_uuid)
             lic.save()
         return subscription_uuid
