a1109d45861e3fdd4a242823dff137b6f00ab2c1
chavesj <jchaves@edx.org>
2020-06-12 14:49:35 -0400

    some more settings cleanup

--- license_manager/settings/local.py
+++ license_manager/settings/local.py
@@ -25,11 +25,6 @@ DATABASES = {
 }
 # END DATABASE CONFIGURATION
 
-# EMAIL CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# END EMAIL CONFIGURATION
-
 # TOOLBAR CONFIGURATION
 # See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 if os.environ.get('ENABLE_DJANGO_TOOLBAR', False):
