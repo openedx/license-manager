029ee4a562afbead2b493253af635d4e61b607e5
Brandon Baker <bbaker@edx.org>
2020-08-24 13:56:11 -0400

    Skip max license validation for test subscriptions
    ENT-3324

--- license_manager/apps/subscriptions/forms.py
+++ license_manager/apps/subscriptions/forms.py
@@ -3,12 +3,13 @@ Forms to be used in the subscriptions django app.
 """
 from django import forms
 
+from license_manager.apps.subscriptions.constants import MAX_NUM_LICENSES
 from license_manager.apps.subscriptions.models import SubscriptionPlan
 
 
 class SubscriptionPlanForm(forms.ModelForm):
     # Extra form field to specify the number of licenses to be associated with the subscription plan
-    num_licenses = forms.IntegerField(label="Number of Licenses", required=False, max_value=1000)
+    num_licenses = forms.IntegerField(label="Number of Licenses", required=False)
 
     def __init__(self, *args, **kwargs):
         super(SubscriptionPlanForm, self).__init__(*args, **kwargs)
@@ -19,8 +20,17 @@ class SubscriptionPlanForm(forms.ModelForm):
         if not super(SubscriptionPlanForm, self).is_valid():
             return False
 
+        form_num_licenses = self.cleaned_data.get('num_licenses', 0)
+        # Only internal use subscription plans to have more than the maximum number of licenses
+        if form_num_licenses > MAX_NUM_LICENSES and not self.instance.for_internal_use_only:
+            self.add_error(
+                'num_licenses',
+                'Non-test subscriptions may not have more than {} licenses'.format(MAX_NUM_LICENSES),
+            )
+            return False
+
         # Ensure the number of licenses is not being decreased
-        if self.cleaned_data.get('num_licenses', 0) < self.instance.num_licenses:
+        if form_num_licenses < self.instance.num_licenses:
             self.add_error('num_licenses', 'Number of Licenses cannot be decreased.')
             return False
 
