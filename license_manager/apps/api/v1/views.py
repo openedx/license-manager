72e6d04d90c30d80cb869199d5c7f7e05bc3ebca
Adam Stankiewicz <agstanki@gmail.com>
2020-10-09 10:05:25 -0400

    add num_pages to licenseviewset

--- license_manager/apps/api/v1/views.py
+++ license_manager/apps/api/v1/views.py
@@ -182,7 +182,18 @@ class LearnerLicenseViewSet(PermissionRequiredForListingMixin, viewsets.ReadOnly
             return None
 
 
-class LicensePagination(PageNumberPagination):
+class PageNumberPaginationWithCount(PageNumberPagination):
+    """
+    A PageNumber paginator that adds the total number of pages to the paginated response.
+    """
+    def get_paginated_response(self, data):
+        """ Adds a ``num_pages`` field into the paginated response. """
+        response = super().get_paginated_response(data)
+        response.data['num_pages'] = self.page.paginator.num_pages
+        return response
+
+
+class LicensePagination(PageNumberPaginationWithCount):
     """
     A PageNumber paginator that allows the client to specify the page size, up to some maximum.
     """
