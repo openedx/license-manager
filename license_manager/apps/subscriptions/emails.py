e99304d3660eafe3dc75e7aec3ee79058856f623
Adam Stankiewicz <agstanki@gmail.com>
2020-10-09 17:12:23 -0400

    WIP email ECS when subs plan reached revocation limit

--- license_manager/apps/subscriptions/emails.py
+++ license_manager/apps/subscriptions/emails.py
@@ -6,13 +6,31 @@ from license_manager.apps.subscriptions.constants import (
     LICENSE_ACTIVATION_EMAIL_SUBJECT,
     LICENSE_ACTIVATION_EMAIL_TEMPLATE,
     LICENSE_REMINDER_EMAIL_SUBJECT,
+    REVOCATION_CAP_NOTIFICATION_EMAIL_SUBJECT,
+    REVOCATION_CAP_NOTIFICATION_EMAIL_TEMPLATE,
 )
+
+
+def send_revocation_cap_notification_email(subscription_plan, enterprise_name):
+    """
+    TODO:
+    """
+    context = {
+        'template_name': REVOCATION_CAP_NOTIFICATION_EMAIL_TEMPLATE,
+        'subject': REVOCATION_CAP_NOTIFICATION_EMAIL_SUBJECT.format(subscription_plan.title),
+        'SUBSCRIPTION_TITLE': subscription_plan.title,
+        'ENTERPRISE_NAME': enterprise_name,
+        'NUM_REVOCATIONS_APPLIED': subscription_plan.num_revocations_applied,
+        'RECIPIENT_EMAIL': settings.CUSTOMER_SUCCESS_EMAIL,
+        'HIDE_EMAIL_FOOTER_MARKETING': True,
+    }
+    email = _message_from_context_and_template(context)
+    email.send()
 
 
 def send_activation_emails(
     custom_template_text,
     pending_licenses,
-    subscription_plan,
     enterprise_slug,
     is_reminder=False
 ):
@@ -32,39 +50,30 @@ def send_activation_emails(
         'template_name': LICENSE_ACTIVATION_EMAIL_TEMPLATE,
         'subject': (LICENSE_ACTIVATION_EMAIL_SUBJECT, LICENSE_REMINDER_EMAIL_SUBJECT)[is_reminder],
         'REMINDER': is_reminder,
+        'TEMPLATE_CLOSING': custom_template_text['closing'],
+        'TEMPLATE_GREETING': custom_template_text['greeting'],
     }
     email_activation_key_map = {}
     for pending_license in pending_licenses:
         email_activation_key_map.update({pending_license.user_email: str(pending_license.activation_key)})
 
     _send_email_with_activation(
-        custom_template_text,
         email_activation_key_map,
-        subscription_plan.expiration_date,
         context,
-        enterprise_slug
+        enterprise_slug,
     )
 
 
-def _send_email_with_activation(
-    custom_template_text,
-    email_activation_key_map,
-    subscription_expiration_date,
-    context,
-    enterprise_slug
-):
+def _send_email_with_activation(email_activation_key_map, context, enterprise_slug):
     """
     Helper that sends emails with the given template with an activation link to the the given list of emails.
 
     Args:
-        custom_template_text (dict): Dictionary containing `greeting` and `closing` keys to be used for customizing
-            the email template.
         email_activation_key_map (dict): Dictionary containing the emails of each recipient and the activation key that
-            is unique to the email. Recipient emails are the keys in the dictionary
-        subscription_expiration_date (datetime): When the subscription expires
+            is unique to the email. Recipient emails are the keys in the dictionary.
         context (dict): Dictionary of context variables for template rendering. Context takes an optional `REMINDER`
             key. If `REMINDER` is provided, a reminder message is added to the email.
-        enterprise_slug (str): The slug associated with an enterprise to uniquely identify it
+        enterprise_slug (str): The slug associated with an enterprise to uniquely identify it.
     """
     # Construct each message to be sent and appended onto the email list
     email_recipient_list = email_activation_key_map.keys()
@@ -77,15 +86,11 @@ def _send_email_with_activation(
                 enterprise_slug,
                 email_activation_key_map.get(email_address)
             ),
-            'USER_EMAIL': email_address,
+            'RECIPIENT_EMAIL': email_address,
             'SOCIAL_MEDIA_FOOTER_URLS': settings.SOCIAL_MEDIA_FOOTER_URLS,
             'MOBILE_STORE_URLS': settings.MOBILE_STORE_URLS,
         })
-        emails.append(_message_from_context_and_template(
-            context,
-            custom_template_text,
-            subscription_expiration_date,
-        ))
+        emails.append(_message_from_context_and_template(context))
 
     # Use a single connection to send all messages
     with mail.get_connection() as connection:
@@ -120,24 +125,18 @@ def _get_rendered_template_content(template_name, extension, context):
     return get_template(message_template).render(context)
 
 
-def _message_from_context_and_template(context, custom_template_text, subscription_expiration_date):
+def _message_from_context_and_template(context):
     """
     Creates a message about the subscription_plan in the template specified by the context, with custom_template_text.
 
     Args:
         context (dict): Dictionary of context variables for template rendering.
-        custom_template_text (dict): Dictionary containing `greeting` and `closing` keys to be used for customizing
-            the email template.
-        subscription_expiration_date (datetime): When the subscription expires
 
     Returns:
         EmailMultiAlternative: an individual message constructed from the information provided, not yet sent
     """
     # Update context to be used for Django template rendering
     context.update({
-        'EXPIRATION_DATE': subscription_expiration_date,
-        'TEMPLATE_CLOSING': custom_template_text['closing'],
-        'TEMPLATE_GREETING': custom_template_text['greeting'],
         'UNSUBSCRIBE_LINK': settings.EMAIL_UNSUBSCRIBE_LINK,
     })
 
@@ -162,9 +161,9 @@ def _message_from_context_and_template(context, custom_template_text, subscripti
         subject=context['subject'],
         body=txt_content,
         from_email=from_email_string,
-        to=[context['USER_EMAIL']],
+        to=[context['RECIPIENT_EMAIL']],
         bcc=[],
-        headers=message_headers
+        headers=message_headers,
     )
     message.attach_alternative(html_content, 'text/html')
     return message
