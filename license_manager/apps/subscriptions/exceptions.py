ca108a011ff500464c88e639581f4b009649790f
Brandon Baker <bbaker@edx.org>
2020-10-30 12:00:39 -0400

    Add some failure state handling

--- license_manager/apps/subscriptions/exceptions.py
+++ license_manager/apps/subscriptions/exceptions.py
@@ -20,3 +20,34 @@ class LicenseRevocationError(Exception):
             self.license_uuid,
             self.failure_reason,
         )
+
+
+class InsufficientLicensesForRenewalError(Exception):
+    """
+    Exception raised during renewal processing when the linked subscription does not have enough unassigned licenses to
+    remove when renewing with a smaller number of licenses.
+
+    Example: A customer has a SubscriptionPlan with 100 licenses, 50 of which are unassigned on the renewal effective
+    date. If they have a renewal being processed that calls for more than 50 licenses, this exception should be raised.
+    """
+    def __init__(self, subscription_for_renewal, num_licenses_to_remove, num_existing_unassigned_licenses):
+        """
+        Args:
+            subscription_for_renewal (SubscriptionPlan): The subscription that was unable to be renewed.
+            num_licenses_to_remove (int): The number of licenses that need to be removed from the subscription to match
+                the number for renewal.
+            num_existing_unassigned_licenses (int): How many unassigned licenses the subscription has prior to renewal.
+        """
+        super().__init__()
+        self.subscription_for_renewal = subscription_for_renewal
+        self.num_licenses_to_remove = num_licenses_to_remove
+        self.num_existing_unassigned_licenses = num_existing_unassigned_licenses
+
+    def __str__(self):
+        message = ('Subscription Renewal failed for SubscriptionPlan with uuid: {}. The renewal calls for {} licenses'
+        ' to be removed, but the subscription only has {} unassigned licenses to remove.')
+        return message.format(
+            self.subscription_for_renewal.uuid,
+            self.num_licenses_to_remove,
+            self.num_existing_unassigned_licenses,
+        )
--- license_manager/apps/subscriptions/management/commands/process_subscription_renewals.py
+++ license_manager/apps/subscriptions/management/commands/process_subscription_renewals.py
@@ -2,7 +2,9 @@ import logging
 from datetime import datetime
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
+from license_manager.apps.subscriptions.exceptions import InsufficientLicensesForRenewalError
 from license_manager.apps.subscriptions.models import (
     SubscriptionPlan,
     SubscriptionPlanRenewal,
@@ -29,52 +31,77 @@ class Command(BaseCommand):
         ).prefetch_related(
             'subscription_plan__licenses'
         )
+        num_renewals_processed = 0
+        mark_job_as_failed = False
 
         for renewal in renewals_for_today:
-            # TODO: Atomic transactions for each one?
             subscription_for_renewal = renewal.subscription_plan
             message = 'Processing renewal with effective date: {} for subscription with uuid: {}'.format(
                 renewal.effective_date,
                 subscription_for_renewal.uuid,
             )
             logger.info(message)
+            # Process each renewal atomically so that a subscription is not changed if there is an error during renewal
+            try:
+                with transaction.atomic():
+                    subscription_for_renewal.start_date = renewal.effective_date
+                    subscription_for_renewal.expiration_date = renewal.renewed_expiration_date
+                    subscription_for_renewal.num_revocations_applied = 0
+                    # Do we need fancier logic around `is_active` here?
+                    subscription_for_renewal.is_active = True
+                    subscription_for_renewal.save()
 
-            subscription_for_renewal.start_date = renewal.effective_date
-            subscription_for_renewal.expiration_date = renewal.renewed_expiration_date
-            subscription_for_renewal.num_revocations_applied = 0
-            # Do we need fancier logic around `is_active` here?
-            subscription_for_renewal.is_active = True
-            subscription_for_renewal.save()
-
-            num_new_licenses = renewal.number_of_licenses - subscription_for_renewal.licenses.count()
-            if num_new_licenses == 0:
-                # If there is no change in licenses, great! Don't do anything
-                pass
-            elif num_new_licenses < 0:
-                # Get the number of unassigned licenses in the subscription for renewal. As long as it's greater than
-                # or equal to the number of new licenses being added, we can just get rid of those unassigned licenses.
-                existing_unassigned_licenses = subscription_for_renewal.unassigned_licenses
-                if existing_unassigned_licenses.count() >= num_new_licenses:
-                    # TODO: Is it ok to delete these licenses? Should they be detached from the subscription? What's the
-                    # best thing from a data preservation standpoint?
-                    # Best thing long term is probably soft-deleting the licenses.
-                    # See: https://medium.com/@adriennedomingus/soft-deletion-in-django-e4882581c340 or similar posts
-                    unassigned_licenses_to_delete = existing_unassigned_licenses[:num_new_licenses]
-                    unassigned_licenses_to_delete.delete()
-                else:
-                    # TODO: Figure out logic on what to do if they are trying to renew but don't have enough unassigned
-                    # to pick from.
-                    print('Not enough unassigned licenses')
-            else:
-                # Another easy situation, they're just adding more licenses
-                subscription_for_renewal.increase_num_licenses(num_new_licenses)
-
+                    # num_new_licenses = renewal.number_of_licenses - subscription_for_renewal.licenses.count()
+                    num_new_licenses = -10
+                    if num_new_licenses == 0:
+                        # If there is no change in licenses, great! Don't do anything
+                        pass
+                    elif num_new_licenses < 0:
+                        num_licenses_to_remove = abs(num_new_licenses)
+                        # Get the number of unassigned licenses in the subscription for renewal. As long as it's greater
+                        # than or equal to the number of new licenses being added, we can just get rid of those
+                        # unassigned licenses.
+                        existing_unassigned_licenses = subscription_for_renewal.unassigned_licenses
+                        if existing_unassigned_licenses.count() >= num_licenses_to_remove:
+                            # TODO: Is it ok to delete these licenses? Should they be detached from the subscription? What's the
+                            # best thing from a data preservation standpoint?
+                            # Best thing long term is probably soft-deleting the licenses.
+                            # See: https://medium.com/@adriennedomingus/soft-deletion-in-django-e4882581c340 or similar posts
+                            unassigned_licenses_to_delete = existing_unassigned_licenses[:num_licenses_to_remove]
+                            # You cannot use 'limit' or 'offset' with delete, so we delete these one at a time in a
+                            # loop. It's not great performance wise, but this is a rare edge case that we don't expect
+                            # to happen on large numbers of licenses.
+                            for unassigned_license in unassigned_licenses_to_delete:
+                                unassigned_license.delete()
+                        else:
+                            # TODO: Notify ECS. Likely needs a ticket for the full handling, email template, etc.
+                            raise InsufficientLicensesForRenewalError(
+                                subscription_for_renewal,
+                                num_licenses_to_remove,
+                                existing_unassigned_licenses.count(),
+                            )
+                    else:
+                        # Another easy situation, they're just adding more licenses
+                        subscription_for_renewal.increase_num_licenses(num_new_licenses)
+            except InsufficientLicensesForRenewalError as exc:
+                logger.error(exc)
+                # We should fail the entire job if one goes wrong, but finish processing the rest first
+                mark_job_as_failed = True
+                continue
+            num_renewals_processed += 1
             message = 'Successfully renewed subscription with uuid: {}'.format(
                 subscription_for_renewal.uuid,
             )
             logger.info(message)
 
-        message = 'Successfully processed {} subscription renewals'.format(
+        message = 'Successfully processed {} out of {} subscription renewal(s)'.format(
+            num_renewals_processed,
             renewals_for_today.count(),
         )
         logger.info(message)
+        if mark_job_as_failed:
+            num_failed_renewals = renewals_for_today.count() - num_renewals_processed
+            error_message = '{} subscription renewal(s) were unable to be processed'.format(
+                num_failed_renewals,
+            )
+            raise Exception(error_message)
