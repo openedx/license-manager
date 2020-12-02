b244162633b3b1cd3a17133e16aadaa028412b8b
Kellie Selinka <>
2020-12-02 15:47:10 -0500

    Increase max number of licenses to 5000

--- license_manager/apps/subscriptions/constants.py
+++ license_manager/apps/subscriptions/constants.py
@@ -42,7 +42,7 @@ SALESFORCE_ID_LENGTH = 18  # The salesforce_opportunity_id must be exactly 18 ch
 DAYS_TO_RETIRE = 90
 
 # Subscription validation constants
-MAX_NUM_LICENSES = 1000
+MAX_NUM_LICENSES = 5000
 
 # SQL bulk operation constants
 LICENSE_BULK_OPERATION_BATCH_SIZE = 100
