5d56eaec2e73aa57ac911ba69cb2cbb6f4f6f9f6
chavesj <jchaves@edx.org>
2021-02-18 16:57:09 -0500

    feat: adding management command to check number of distinct catalog queries used by SubscriptionPlan catalogs

---
+++ license_manager/apps/subscriptions/management/commands/validate_num_catalog_queries.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from license_manager.apps.subscriptions.tasks import validate_query_mapping_task
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = (
+        'Verify that the correct number of distinct Catalog Queries used by Enterprise Catalogs for '
+        'all SubscriptionPlans in the license-manager service exist in the enterprise-catalog service.'
+    )
+
+    def handle(self, *args, **options):
+        validate_query_mapping_task.delay()
---
+++ license_manager/apps/subscriptions/tasks.py
@@ -0,0 +1,35 @@
+import logging
+
+from celery import shared_task
+from celery_utils.logged_task import LoggedTask
+
+from license_manager.apps.api_client.enterprise_catalog import EnterpriseCatalogApiClient
+from license_manager.apps.subscriptions.models import SubscriptionPlan
+
+logger = logging.getLogger(__name__)
+
+
+@shared_task(base=LoggedTask)
+def validate_query_mapping_task():
+    """
+    Task to validate the number of catalog queries used across
+    Enterprise Catalogs assigned to customer SubscriptionPlans.
+    """
+    all_catalog_uuids = list(map(
+        lambda x: str(x.enterprise_catalog_uuid),
+        SubscriptionPlan.objects.filter(
+            for_internal_use_only=False,
+        ),
+    ))
+    api_client = EnterpriseCatalogApiClient()
+    response = api_client.get_distinct_catalog_queries(all_catalog_uuids)
+    summary = '{} distinct catalog queries found: {}'.format(
+        response['count'],
+        response['catalog_query_ids'],
+    )
+    if response['count'] != 2:
+        error_msg = 'ERROR: Unexpected number of Subscription Catalog Queries found. ' + summary
+        logger.error(error_msg)
+        # TODO: Send email to ECS
+    else:
+        logger.info('SUCCESS: ' + summary)
