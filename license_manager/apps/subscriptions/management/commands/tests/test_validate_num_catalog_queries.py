2ea7dd09220bc77de40eab00ba76a41ad7f75a39
chavesj <jchaves@edx.org>
2021-02-24 09:54:05 -0500

    test fix

--- license_manager/apps/subscriptions/management/commands/tests/test_validate_num_catalog_queries.py
+++ license_manager/apps/subscriptions/management/commands/tests/test_validate_num_catalog_queries.py
@@ -18,46 +18,37 @@ class ValidateQueryMappingTaskTests(TestCase):
     """
     command_name = 'validate_num_catalog_queries'
 
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        cls.sub_plan = SubscriptionPlanFactory()
-
-    @ddt.unpack
     @ddt.data(
-        {
-            'num_queries_found': settings.NUM_SUBSCRIPTION_CUSTOMER_TYPES - 1,
-            'is_valid': False,
-        },
-        {
-            'num_queries_found': settings.NUM_SUBSCRIPTION_CUSTOMER_TYPES,
-            'is_valid': True,
-        },
+        False,
+        True,
     )
     @mock.patch(
         'license_manager.apps.subscriptions.management.commands.validate_num_catalog_queries.EnterpriseCatalogApiClient'
     )
-    def test_email_sent_for_invalid_num_queries(self, mock_api_client, num_queries_found, is_valid):
+    def test_email_sent_for_invalid_num_queries(self, is_valid, mock_api_client):
         """
         Tests that an email is sent to ECS in the case that an invalid number of distinct
         CatalogQuery IDs are found to be used for all subscription customers.
 
         Tests that no email is sent if the valid number of distinct CatalogQuery IDs are found.
         """
-        log_level = 'INFO' if is_valid else 'ERROR'
+        # Arbitrary number of subscriptions ("correct" number)
+        num_subs = 3
+        SubscriptionPlanFactory.create_batch(num_subs)
+        if is_valid:
+            log_level = 'INFO'
+            num_queries_found = num_subs
+        else:
+            log_level = 'ERROR'
+            num_queries_found = num_subs - 1
         with self.assertLogs(level=log_level) as log:
-            catalog_query_ids = []
-            for _ in range(num_queries_found):
-                catalog_query_ids.append(str(uuid.uuid4()))
-            mock_response = {
+            catalog_query_ids = [str(uuid.uuid4()) for _ in range(num_queries_found)]
+            mock_api_client.return_value.get_distinct_catalog_queries.return_value = {
                 'count': num_queries_found,
                 'catalog_query_ids': catalog_query_ids,
             }
-            mock_api_client.return_value.get_distinct_catalog_queries.return_value = mock_response
             call_command(self.command_name)
             if is_valid:
-                # Assert SUCCESS logged
                 assert 'SUCCESS' in log.output[0]
             else:
-                # Assert ERROR logged
                 assert 'ERROR' in log.output[0]
