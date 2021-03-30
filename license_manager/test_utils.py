9ea12db57e7d0dee89181d04470a70bb8cdaec1d
Alex Dusenbery <adusenbery@edx.org>
2021-03-30 15:43:17 -0400

    feat: Send lists of user emails to the pending enterprise user endpoint, instead of one email per request, so that we don't get rate-limited

--- license_manager/test_utils.py
+++ license_manager/test_utils.py
@@ -1,12 +1,16 @@
 """
 Testing utilities for license-manager
 """
+import requests
 
 
-class MockResponse:
-    def __init__(self, json_data, status_code):
+class MockResponse(requests.Response):
+    def __init__(self, json_data, status_code, content=None):
+        super().__init__()
+
         self.json_data = json_data
         self.status_code = status_code
+        self._content = content
 
-    def json(self):
+    def json(self):  # pylint: disable=arguments-differ
         return self.json_data
