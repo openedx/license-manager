6c9b3b3ed1bbc956c96e0b73eee36fb50e810f93
kiram15 <kmiller@edx.org>
2021-06-16 11:50:00 -0400

    adding save to draft

--- license_manager/apps/subscriptions/management/commands/populate_plan_types.py
+++ license_manager/apps/subscriptions/management/commands/populate_plan_types.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from license_manager.apps.subscriptions.models import SubscriptionPlan, PlanType
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -8,14 +12,18 @@ class Command(BaseCommand):
         'Automatically populates the subscription plan types using the netsuite product id.'
     )
 
-    for row in SubscriptionPlan.objects.all():
-        if row.netsuite_product_id == 0:
-            plan = PlanType.objects.filter(label='OCE')
-            row.PlanType = plan
-        elif row.netsuite_product_id == 106 or row.netsuite_product_id == 110:
-            plan = PlanType.objects.filter(label='Standard Paid')
-            row.PlanType = plan
-        else:
-            plan = PlanType.objects.filter(label='Test')
-            row.PlanType = plan
-        
+    def handle(self, *args, **options):
+        for row in SubscriptionPlan.objects.all():
+            if row.netsuite_product_id == 0:
+                Command.save_plan(row, 'OCE')
+            elif row.netsuite_product_id == 106 or row.netsuite_product_id == 110:
+                Command.save_plan(row, 'Standard Paid')
+            else:
+                Command.save_plan(row, 'Test')
+
+    def save_plan(row, label):
+        plan = PlanType.objects.filter(label='OCE')
+        row.PlanType = plan
+        row.save()
+        message = 'Assigned {} label to subscription plan \"{}\"'.format(label, row.title)
+        logger.info(message)
