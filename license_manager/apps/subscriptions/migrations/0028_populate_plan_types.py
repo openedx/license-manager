a9c8c41ccdcd0da8750f79291e4dae3cd8c95850
kiram15 <kmiller@edx.org>
2021-06-16 14:37:55 -0400

    adding in changes to get working migrations

---
+++ license_manager/apps/subscriptions/migrations/0028_populate_plan_types.py
@@ -0,0 +1,34 @@
+import logging
+
+from django.db import migrations, models
+
+logger = logging.getLogger(__name__)
+
+def save_plan(row, plan_label, apps):
+    PlanType = apps.get_model("subscriptions", "PlanType")
+    message = 'Assigned {} label to subscription plan \"{}\"'.format(plan_label, row.title)
+    logger.info(message)
+    return PlanType.objects.get(label=plan_label)
+
+def populate_plan(apps, schema_editor):
+    SubscriptionPlan = apps.get_model('subscriptions', 'SubscriptionPlan')
+    for row in SubscriptionPlan.objects.all():
+        if row.netsuite_product_id == 0:
+            plan = save_plan(row, 'OCE', apps)
+        elif row.netsuite_product_id == 106 or row.netsuite_product_id == 110:
+            plan = save_plan(row, 'Standard Paid', apps)
+        else:
+            plan = save_plan(row, 'Test', apps)
+        row.PlanType = plan
+        row.save()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('subscriptions', '0027_renewal_cap_toggle'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_plan),
+    ]
+
+
