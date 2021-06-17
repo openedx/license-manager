4687553abf10edb74435044ece1c61072ff34db8
kiram15 <kmiller@edx.org>
2021-06-17 13:13:43 -0400

    adding rollback capability

--- license_manager/apps/subscriptions/migrations/0029_populate_plan_types.py
+++ license_manager/apps/subscriptions/migrations/0029_populate_plan_types.py
@@ -1,4 +1,8 @@
+import logging
+
 from django.db import migrations
+
+logger = logging.getLogger(__name__)
 
 def populate_plan(apps, schema_editor):
     SubscriptionPlan = apps.get_model('subscriptions', 'SubscriptionPlan')
@@ -7,10 +11,17 @@ def populate_plan(apps, schema_editor):
         if row.netsuite_product_id == 0:
             plan = PlanType.objects.get(label='OCE')
         elif row.netsuite_product_id == 106 or row.netsuite_product_id == 110:
-            plan = PlanType.objects.get(label='Standard Paid')        
+            plan = PlanType.objects.get(label='Standard Paid') 
         else:
             plan = PlanType.objects.get(label='Test')
-        row.PlanType = plan
+        row.plan_type = plan
+        row.save()
+
+def depopulate_plan(apps, schema_editor):
+    SubscriptionPlan = apps.get_model('subscriptions', 'SubscriptionPlan')
+    PlanType = apps.get_model("subscriptions", "PlanType")
+    for row in SubscriptionPlan.objects.all():
+        row.plan_type = None
         row.save()
 
 class Migration(migrations.Migration):
@@ -19,5 +30,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(populate_plan),
+        migrations.RunPython(populate_plan, depopulate_plan),
     ]
