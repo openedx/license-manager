4b035441e16dfc6d67a5971bedb96b8d0c4ab897
Kira Miller <31229189+kiram15@users.noreply.github.com>
2021-06-21 09:31:57 -0400

    Backfill of plan_type_id value (#227)
    * populate plan type values in database

---
+++ license_manager/apps/subscriptions/migrations/0029_populate_plan_types.py
@@ -0,0 +1,29 @@
+from django.db import migrations
+
+def populate_plan(apps, schema_editor):
+    SubscriptionPlan = apps.get_model('subscriptions', 'SubscriptionPlan')
+    PlanType = apps.get_model("subscriptions", "PlanType")
+    for row in SubscriptionPlan.objects.all():
+        if row.netsuite_product_id == 0:
+            plan = PlanType.objects.get(label='OCE')
+        elif row.netsuite_product_id == 106 or row.netsuite_product_id == 110:
+            plan = PlanType.objects.get(label='Standard Paid') 
+        else:
+            plan = PlanType.objects.get(label='Test')
+        row.plan_type = plan
+        row.save()
+
+def depopulate_plan(apps, schema_editor):
+    SubscriptionPlan = apps.get_model('subscriptions', 'SubscriptionPlan')
+    for row in SubscriptionPlan.objects.all():
+        row.plan_type = None
+        row.save()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('subscriptions', '0028_plan_type_help_text'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_plan, depopulate_plan),
+    ]
