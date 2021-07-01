92da19ebe0310eefb69eb8024510e08c036deffe
kiram15 <kmiller@edx.org>
2021-07-01 10:45:00 -0400

    talia is always right and i will never question her again

--- license_manager/apps/api/v1/tests/test_views.py
+++ license_manager/apps/api/v1/tests/test_views.py
@@ -2494,7 +2494,7 @@ class LicenseActivationViewTests(LicenseViewTestMixin, TestCase):
             jwt_payload_extra={
                 'user_id': self.lms_user_id,
                 'email': self.user.email,
-                'subscription_uuid': self.active_subscription_for_customer,
+                'subscription_uuid': str(self.active_subscription_for_customer.uuid),
             }
         )
         license_to_be_activated = self._create_license()
