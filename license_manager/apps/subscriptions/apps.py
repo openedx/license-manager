8904f3dbbb4c3b8c313a2b54938e5cb748a37b60
Brian Beggs <macdiesel@gmail.com>
2021-08-12 15:25:51 -0400

    feat: Setup segment key read from configuration

--- license_manager/apps/subscriptions/apps.py
+++ license_manager/apps/subscriptions/apps.py
@@ -1,6 +1,20 @@
+import analytics
+
 from django.apps import AppConfig
+from django.conf import settings
+
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class SubscriptionsConfig(AppConfig):
-    name = 'subscriptions'
+    name = 'license_manager.apps.subscriptions'
     default = False
+
+    def ready(self):
+        if settings.SEGMENT_KEY:
+            logger.debug("Found segment key, setting up")
+            analytics.write_key = settings.SEGMENT_KEY
+
