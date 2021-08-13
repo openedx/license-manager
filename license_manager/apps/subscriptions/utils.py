0407e5089e5562d39607b4b35b3271d9cbfffe3f
Brian Beggs <macdiesel@gmail.com>
2021-08-13 08:18:04 -0400

    feat: Add utility method to fire segment events and fetch CustomerAgreement details

--- license_manager/apps/subscriptions/utils.py
+++ license_manager/apps/subscriptions/utils.py
@@ -1,4 +1,5 @@
 """ Utility functions for the subscriptions app. """
+import analytics
 from datetime import date, datetime
 
 from django.conf import settings
@@ -7,6 +8,12 @@ from pytz import UTC
 from license_manager.apps.subscriptions.constants import (
     DEFAULT_EMAIL_SENDER_ALIAS,
 )
+import logging
+
+from license_manager.apps.subscriptions.models import CustomerAgreement
+
+
+logger = logging.getLogger(__name__)
 
 
 # pylint: disable=no-value-for-parameter
@@ -78,3 +85,47 @@ def get_enterprise_reply_to_email(enterprise_customer):
     Returns the configured reply_to email for an enterprise, if configured.
     """
     return enterprise_customer.get('reply_to') or ''
+
+def track_event(user_id, event_name, properties):
+    """
+    Send a tracking event to segment
+
+    Args:
+        user_id (str): ID of the user who generated this event
+        event_name (str): Name of the event in the format of: edx.server.license-manager.license-lifecycle.<new-status>
+        properties (dict): All the properties of an event. See docs/segment_events.rst
+
+    Returns:
+        None
+    """
+    if settings.SEGMENT_KEY:
+        try: # We should never raise an exception when not able to send a tracking event
+            analytics.track(user_id, event_name, properties)
+        except Exception: # pylint: disable=broad-except
+            logger.exception()
+    else:
+        logger.debug("Event {} for user_id {} not tracked because SEGMENT_KEY not set". format(event_name, user_id))
+
+def get_enterprise_tracking_properties(enterprise_customer_uuid):
+    """
+    Get the UUIDs from the database about the enterprise CustomerAgreement
+
+    Args:
+        enterprise_customer_uuid (str): UUID of the enterprise customer
+
+    Returns:
+        a (dict) containing the UUIDs in the customer_agreement model.
+    """
+    try:
+        customer_agreement = CustomerAgreement.objects.get(enterprise_customer_uuid=enterprise_customer_uuid)
+        return {
+            'enterprise_customer_uuid': enterprise_customer_uuid,
+            'customer_agreement_uuid': customer_agreement.uuid,
+            'enterprise_customer_slug': customer_agreement.enterprise_customer_slug,
+            'default_enterprise_catalog_uuid': customer_agreement.default_enterprise_catalog_uuid
+        }
+    except CustomerAgreement.DoesNotExist:
+        logger.debug("Unable to find a CustomerAgreement for UUID: {}".format(enterprise_customer_uuid))
+        return {}
+    except Exception: # pylint: disable=broad-except
+        logger.exception()
