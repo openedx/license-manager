ab2c383880e334f99dc07ee0233c942167e2e20f
Chris Pappas <cpappas@edx.org>
2021-10-29 11:58:53 -0400

    chore: adding segment event to no licenses left on auto apply

--- license_manager/apps/subscriptions/constants.py
+++ license_manager/apps/subscriptions/constants.py
@@ -64,6 +64,7 @@ class SegmentEvents:
     LICENSE_EXPIRED = 'edx.server.license-manager.license-lifecycle.expired'
     LICENSE_RENEWED = 'edx.server.license-manager.license-lifecycle.renewed'
     LICENSE_REVOKED = 'edx.server.license-manager.license-lifecycle.revoked'
+    LICENSE_NOT_CREATED = 'edx.server.license-manager.license-lifecycle.not-created'
 
 
 # Template names used for emails
