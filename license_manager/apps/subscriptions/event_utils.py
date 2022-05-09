0f24460136c7756b608e537264c0cd4ce940f265
Rebecca Graber <rgraber@edx.org>
2022-05-09 12:56:11 -0400

    fixup! :pylint

--- license_manager/apps/subscriptions/event_utils.py
+++ license_manager/apps/subscriptions/event_utils.py
@@ -184,7 +184,7 @@ def track_event(lms_user_id, event_name, properties):
     if KAFKA_ENABLED.is_enabled():  # pragma: no cover
         try:
             # assigned_lms_user_id expects an integer or None, but elsewhere it is defaulted to ''
-            properties_copy = {key: value for key, value in properties.items()}
+            properties_copy = dict(properties.items())
             if properties_copy['assigned_lms_user_id'] == "":
                 properties_copy['assigned_lms_user_id'] = None
             license_data = SubscriptionLicenseData(**properties_copy)
