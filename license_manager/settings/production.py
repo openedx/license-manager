41465befbf66c7063f45de5fa1e5ff9aa5458419
Alex Dusenbery <adusenbery@edx.org>
2023-01-18 09:08:17 -0500

    feat: set user throttling thresholds higher, REST_FRAMEWORK updated from yaml settings.

--- license_manager/settings/production.py
+++ license_manager/settings/production.py
@@ -14,7 +14,7 @@ LOGGING = get_logger_config()
 
 # Keep track of the names of settings that represent dicts. Instead of overriding the values in base.py,
 # the values read from disk should UPDATE the pre-configured dicts.
-DICT_UPDATE_KEYS = ('JWT_AUTH',)
+DICT_UPDATE_KEYS = ('JWT_AUTH', 'REST_FRAMEWORK')
 
 # This may be overridden by the YAML in license_manager_CFG,
 # but it should be here as a default.
