664b16e3d4ace2fe69d6f600a1dca828cd964582
Ehmad Saeed⚡ <justehmadsaeed@gmail.com>
2024-03-13 17:58:21 +0500

    fix: trailing slash conflict for api endpoints (#616)

--- license_manager/apps/api/urls.py
+++ license_manager/apps/api/urls.py
@@ -4,12 +4,20 @@ Root API URLs.
 All API URLs should be versioned, so urlpatterns should only
 contain namespaces for the active versions of the API.
 """
+import re
+
 from django.urls import include, path
 
 from license_manager.apps.api.v1 import urls as v1_urls
 
 
+def optional_trailing_slash(urls):
+    for url in urls[0].urlpatterns:
+        url.pattern.regex = re.compile(url.pattern.regex.pattern.replace('/$', '/?$'))
+    return urls
+
+
 app_name = 'api'
 urlpatterns = [
-    path('v1/', include(v1_urls)),
+    path('v1/', optional_trailing_slash(include(v1_urls))),
 ]
