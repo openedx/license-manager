af2591f19aaf24af5354ff27ed7e86d9d21d0116
hamzawaleed01 <hamza.waleed@arbisoft.com>
2024-05-20 18:17:33 +0500

    feat: WIP - implement access control for license provisioning

--- license_manager/apps/api/permissions.py
+++ license_manager/apps/api/permissions.py
@@ -13,3 +13,20 @@ class CanRetireUser(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.username == settings.RETIREMENT_SERVICE_WORKER_USERNAME or request.user.is_superuser
+
+
+class CanProvisionLicenses(permissions.BasePermission):
+    """
+    Grant access to the user retirement API for the service user, and to superusers. This mimics the
+    retirement permissions check in edx-platform.
+    """
+    ALLOWED_API_GROUPS = ['license_provisioning_admins']
+    message = 'User is not allowed to access the view.'
+
+    def has_permission(self, request, view):
+        return (
+            super().has_permission(request, view) and (
+                request.user.groups.filter(name__in=self.ALLOWED_API_GROUPS).exists()
+            )
+        )
+    
