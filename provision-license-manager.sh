5691e5fd1bf7ded023ed09edb3346c00baf12261
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2024-11-05 16:53:57 +0500

    chore: removed Dockerfile
    update docker-compose to pull from edxops repo

--- provision-license-manager.sh
+++ provision-license-manager.sh
@@ -1,7 +1,7 @@
 name="license_manager"
 port="18170"
 
-docker-compose up -d --build
+docker-compose up -d
 
 # Install requirements
 # Can be skipped right now because we're using the --build flag on docker-compose. This will need to be changed once we move to devstack.
