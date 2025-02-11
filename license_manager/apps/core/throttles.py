11312949ae209a28c456f7b3cf70fadf3d60119c
Alexander Dusenbery <adusenbery@2u.com>
2025-02-11 10:15:39 -0500

    feat: disable throttling for priveleged users
    Introduces a setting PRIVELEGED_USER_IDS, such that any user
    id in the list is exempt from throttling limits.

--- license_manager/apps/core/throttles.py
+++ license_manager/apps/core/throttles.py
@@ -3,13 +3,28 @@ Custom DRF user-throttling classes
 so that we can throttle both bursty and sustained
 throughtput.
 """
-
+from django.conf import settings
 from rest_framework.throttling import UserRateThrottle
 
 
-class UserBurstRateThrottle(UserRateThrottle):
+class PrivelegedUserThrottle(UserRateThrottle):
+    """
+    Skips throttling is the requesting authenticated user
+    is in the list of priveleged user ids.
+    is staff or superuser.
+    """
+    def allow_request(self, request, view):
+        user = request.user
+
+        if user and user.is_authenticated and user.id in settings.PRIVELEGED_USER_IDS:
+            return True
+
+        return super().allow_request(request, view)
+
+
+class UserBurstRateThrottle(PrivelegedUserThrottle):
     scope = 'user_burst'
 
 
-class UserSustainedRateThrottle(UserRateThrottle):
+class UserSustainedRateThrottle(PrivelegedUserThrottle):
     scope = 'user_sustained'
